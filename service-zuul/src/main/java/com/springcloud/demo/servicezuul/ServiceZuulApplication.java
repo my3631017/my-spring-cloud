048dd971f850f903083fe308c3e283b125e4b6b2
pobing <pobing@bicitech.cn>
2018-11-29 16:55:10 +0800

    gateway和zuul

--- service-zuul/src/main/java/com/springcloud/demo/servicezuul/ServiceZuulApplication.java
+++ service-zuul/src/main/java/com/springcloud/demo/servicezuul/ServiceZuulApplication.java
@@ -2,8 +2,14 @@ package com.springcloud.demo.servicezuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableZuulProxy
 public class ServiceZuulApplication {
 
 	public static void main(String[] args) {
